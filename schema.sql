9a379d938cf62f28c347b52ba345e52fb10106a9
Ariel Marchand <marchand.aa@gmail.com>
2021-05-07 20:47:42 -0600

    first push

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE database employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30)  UNIQUE NOT NULL,
+last_name VARCHAR(30) UNIQUE NOT NULL,
+role_id INT NULL,
+manager_id INT,
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES manager(id),
+
+);
+
+CREATE TABLE role (
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(6,2),
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department(id),
+);
+
+CREATE TABLE department (
+id INT AUTO_INCRIMENT PRIMARY KEY,
+name VARCHAR(30) UNIQUE NOT NULL
+);
